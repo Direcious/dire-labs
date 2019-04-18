cd3cc1ebde088e1dac9ad045eae2dbafa9c61a1b
Branden Maleki <branden@maleki.io>
2019-04-17 21:09:25 -0400

    Updated SVGs and syntax

--- src/188vert.cpp
+++ src/188vert.cpp
@@ -35,93 +35,78 @@ _188vertWidget::_188vertWidget(_188vert *module) : ModuleWidget(module) {
 
   addChild(createWidget<ScrewSilver>(Vec(15, 0)));
   addChild(createWidget<ScrewSilver>(Vec(box.size.x - 30, 0)));
-  addChild(createWidget<ScrewSilver>(Vec(15, 365)));
-  addChild(createWidget<ScrewSilver>(Vec(box.size.x - 30, 365)));
+  addChild(createWidget<ScrewSilver>(Vec(15, box.size.y - 15)));
+  addChild(createWidget<ScrewSilver>(Vec(box.size.x - 30, box.size.y - 15)));
 
-  addInput(Port::create<PJ301MPort>(Vec(9.507, 17), Port::INPUT, module, 0));
+  addInput(createInputCentered<PJ301MPort>(
+      Vec(box.size.x / 2, box.size.y - 348), module, 0));
 
-  addParam(ParamWidget::create<RoundBlackKnob>(Vec(45.308, 47.753), module, 0,
-                                               0.0f, 1.0f, 0.0f));
-  addParam(ParamWidget::create<RoundBlackKnob>(Vec(45.308, 86.198), module, 1,
-                                               0.0f, 1.0f, 0.0f));
-  addParam(ParamWidget::create<RoundBlackKnob>(Vec(45.308, 124.639), module, 2,
-                                               0.0f, 1.0f, 0.0f));
-  addParam(ParamWidget::create<RoundBlackKnob>(Vec(45.308, 163.084), module, 3,
-                                               0.0f, 1.0f, 0.0f));
-  addParam(ParamWidget::create<RoundBlackKnob>(Vec(45.308, 201.529), module, 4,
-                                               0.0f, 1.0f, 0.0f));
-  addParam(ParamWidget::create<RoundBlackKnob>(Vec(45.308, 239.974), module, 5,
-                                               0.0f, 1.0f, 0.0f));
-  addParam(ParamWidget::create<RoundBlackKnob>(Vec(45.308, 278.415), module, 6,
-                                               0.0f, 1.0f, 0.0f));
-  addParam(ParamWidget::create<RoundBlackKnob>(Vec(45.308, 316.86), module, 7,
-                                               0.0f, 1.0f, 0.0f));
+  addParam(createParam<RoundBlackKnob>(Vec(45.308, 47.753), module, 0, 0.0f,
+                                       1.0f, 0.0f));
+  addParam(createParam<RoundBlackKnob>(Vec(45.308, 86.198), module, 1, 0.0f,
+                                       1.0f, 0.0f));
+  addParam(createParam<RoundBlackKnob>(Vec(45.308, 124.639), module, 2, 0.0f,
+                                       1.0f, 0.0f));
+  addParam(createParam<RoundBlackKnob>(Vec(45.308, 163.084), module, 3, 0.0f,
+                                       1.0f, 0.0f));
+  addParam(createParam<RoundBlackKnob>(Vec(45.308, 201.529), module, 4, 0.0f,
+                                       1.0f, 0.0f));
+  addParam(createParam<RoundBlackKnob>(Vec(45.308, 239.974), module, 5, 0.0f,
+                                       1.0f, 0.0f));
+  addParam(createParam<RoundBlackKnob>(Vec(45.308, 278.415), module, 6, 0.0f,
+                                       1.0f, 0.0f));
+  addParam(createParam<RoundBlackKnob>(Vec(45.308, 316.86), module, 7, 0.0f,
+                                       1.0f, 0.0f));
 
-  addOutput(
-      Port::create<PJ301MPort>(Vec(9.507, 50.397), Port::OUTPUT, module, 0));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(86.393, 50.397), Port::OUTPUT, module, 1));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(9.507, 88.842), Port::OUTPUT, module, 2));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(86.393, 88.842), Port::OUTPUT, module, 3));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(9.507, 127.283), Port::OUTPUT, module, 4));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(86.393, 127.283), Port::OUTPUT, module, 5));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(9.507, 165.728), Port::OUTPUT, module, 6));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(86.393, 165.728), Port::OUTPUT, module, 7));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(9.507, 204.173), Port::OUTPUT, module, 8));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(86.393, 204.173), Port::OUTPUT, module, 9));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(9.507, 242.614), Port::OUTPUT, module, 10));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(86.393, 242.614), Port::OUTPUT, module, 11));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(9.507, 281.059), Port::OUTPUT, module, 12));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(86.393, 281.059), Port::OUTPUT, module, 13));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(9.507, 319.504), Port::OUTPUT, module, 14));
-  addOutput(
-      Port::create<PJ301MPort>(Vec(86.393, 319.504), Port::OUTPUT, module, 15));
+  addOutput(createOutput<PJ301MPort>(Vec(9.507, 50.397), module, 0));
+  addOutput(createOutput<PJ301MPort>(Vec(86.393, 50.397), module, 1));
+  addOutput(createOutput<PJ301MPort>(Vec(9.507, 88.842), module, 2));
+  addOutput(createOutput<PJ301MPort>(Vec(86.393, 88.842), module, 3));
+  addOutput(createOutput<PJ301MPort>(Vec(9.507, 127.283), module, 4));
+  addOutput(createOutput<PJ301MPort>(Vec(86.393, 127.283), module, 5));
+  addOutput(createOutput<PJ301MPort>(Vec(9.507, 165.728), module, 6));
+  addOutput(createOutput<PJ301MPort>(Vec(86.393, 165.728), module, 7));
+  addOutput(createOutput<PJ301MPort>(Vec(9.507, 204.173), module, 8));
+  addOutput(createOutput<PJ301MPort>(Vec(86.393, 204.173), module, 9));
+  addOutput(createOutput<PJ301MPort>(Vec(9.507, 242.614), module, 10));
+  addOutput(createOutput<PJ301MPort>(Vec(86.393, 242.614), module, 11));
+  addOutput(createOutput<PJ301MPort>(Vec(9.507, 281.059), module, 12));
+  addOutput(createOutput<PJ301MPort>(Vec(86.393, 281.059), module, 13));
+  addOutput(createOutput<PJ301MPort>(Vec(9.507, 319.504), module, 14));
+  addOutput(createOutput<PJ301MPort>(Vec(86.393, 319.504), module, 15));
 
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(30.814, 50.414), module, 0));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(107.702, 50.414), module, 2));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(30.814, 88.859), module, 4));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(107.702, 88.859), module, 6));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(30.814, 127.304), module, 8));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(107.702, 127.304), module, 10));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(30.814, 165.745), module, 12));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(107.702, 165.745), module, 14));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(30.814, 204.19), module, 16));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(107.702, 204.19), module, 18));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(30.814, 242.635), module, 20));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(107.702, 242.635), module, 22));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(30.814, 281.076), module, 24));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(107.702, 281.076), module, 26));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(30.814, 319.521), module, 28));
-  addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(
-      Vec(107.702, 319.521), module, 30));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(30.814, 50.414),
+                                                         module, 0));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(107.702, 50.414),
+                                                         module, 2));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(30.814, 88.859),
+                                                         module, 4));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(107.702, 88.859),
+                                                         module, 6));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(30.814, 127.304),
+                                                         module, 8));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(107.702, 127.304),
+                                                         module, 10));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(30.814, 165.745),
+                                                         module, 12));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(107.702, 165.745),
+                                                         module, 14));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(30.814, 204.19),
+                                                         module, 16));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(107.702, 204.19),
+                                                         module, 18));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(30.814, 242.635),
+                                                         module, 20));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(107.702, 242.635),
+                                                         module, 22));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(30.814, 281.076),
+                                                         module, 24));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(107.702, 281.076),
+                                                         module, 26));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(30.814, 319.521),
+                                                         module, 28));
+  addChild(createLightCentered<TinyLight<GreenRedLight>>(Vec(107.702, 319.521),
+                                                         module, 30));
 }
 
 Model *model_188vert =
--- src/brokenclocks.cpp
+++ src/brokenclocks.cpp
@@ -37,12 +37,8 @@ _brokenclocksWidget::_brokenclocksWidget(_brokenclocks *module)
     : ModuleWidget(module) {
   setPanel(SVG::load(assetPlugin(plugin, "res/brokenclocks.svg")));
 
-  addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-  addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-  addChild(createWidget<ScrewSilver>(
-      Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-  addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH,
-                                         RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+  addChild(createWidget<ScrewSilver>(Vec(15, 0)));
+  addChild(createWidget<ScrewSilver>(Vec(15, box.size.y - 15)));
 
   addInput(createInputCentered<PJ301MPort>(
       Vec(box.size.x / 2, box.size.y - 340), module, 0));
